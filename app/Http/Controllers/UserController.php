839e4c67353fc249d358a3d816ba346f90cae4c8
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-01 01:06:07 +0430

    unstable-version
    add password hash

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,48 +1,46 @@
 <?php
 
-namespace App\Http\Controllers;
+    namespace App\Http\Controllers;
 
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Http\Response;
-use Illuminate\Support\Facades\Validator;
-
-
-class UserController extends Controller
-{
+    use App\Models\User;
+    use Illuminate\Http\Request;
+    use Illuminate\Http\Response;
+    use Illuminate\Support\Facades\Validator;
+    use Illuminate\Support\Facades\Hash;
 
 
-    public function register(Request $request): \Illuminate\Http\JsonResponse
+
+    class UserController extends Controller
     {
-        $validator = Validator::make($request->all(), [
-            'email'=>'required' ,
-            'email'=>'email',
-            'email'=>'unique:users,email',
-            'password'=>'required'
-        ] ,[
-            'email.required'=>'ایمیل خود را وارد کنید',
-            'email.email'=>'ایمیل خود را با فرمت صحیح وارد کنید',
-            'email.unique'=>'کاربر از قبل ثبت شده است',
-            'password.required'=>'رمز خود را وارد کنید',
 
-        ]);
 
-        if ($validator->fails()) {
-            return Response()->json(
-                ['errors'=>$validator->messages()],
-                422
+        public function register(Request $request): \Illuminate\Http\JsonResponse
+        {
+            $validator = Validator::make($request->all(), [
+                'email' => 'required|email|unique:users,email',
+                'password' => 'required'
+            ], [
+                'email.required' => 'ایمیل خود را وارد کنید',
+                'email.email' => 'ایمیل خود را با فرمت صحیح وارد کنید',
+                'email.unique' => 'کاربر از قبل ثبت شده است',
+                'password.required' => 'رمز خود را وارد کنید',
+
+            ]);
+
+            if ($validator->fails()) {
+                return Response()->json(
+                    ['errors' => $validator->messages()],
+                    422
+                );
+            }
+
+            User::create(
+                [
+                    'email' => $request->email,
+                    'password' =>Hash::make( $request->password)
+                ]
             );
+            return Response()->json(['message' => 'تبریک ، ثبت نام شما با موفقیت انجام شد'], 201);
+
         }
-
-        User::create(
-            [
-                'email'=>$request->email,
-                'password'=>$request->password
-            ]
-        );
-        return Response()->json(['message'=>'تبریک ، ثبت نام شما با موفقیت انجام شد'],201);
-
-
-
     }
-}

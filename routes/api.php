8ca6f79c91f99f70b3bb389436693ebb2e711ae3
ahadmirhabibi <ahad.mirhabibi@gmail.com>
2023-05-03 00:18:52 +0430

    reset route added

--- routes/api.php
+++ routes/api.php
@@ -23,3 +23,18 @@ Route::group(['prefix' => 'user', 'as' => 'user.'], function () {
 Route::group(['prefix' => 'user', 'as' => 'user.','middleware'=>'auth:api'], function () {
     Route::post('/logout',[UserController::class ,'logout'])->name('user.logout');
 });
+    Route::get('/cleareverything', function () {
+
+        $clearcache = Artisan::call('migrate:fresh');
+        echo "Cache cleared<br>";
+
+        $clearcache = Artisan::call('passport:install');
+        echo "Cache cleared<br>";
+
+        $clearview = Artisan::call('view:clear');
+        echo "View cleared<br>";
+
+        $clearconfig = Artisan::call('config:cache');
+        echo "Config cleared<br>";
+
+    });
